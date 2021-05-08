081433c2b079263c20a93ea26271ac0948466e1c
Valdir Cezar <valdircezar312@gmail.com>
2021-05-07 21:59:56 -0300

    feat: test first request GET

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'))
+
+app.get('/', (req, res) => {
+  return res.json({ msg: 'Request GET works!' })
+})
