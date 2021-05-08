77395c56258b0a472bb09836ebbee5c79bbe3967
Valdir Cezar <valdircezar312@gmail.com>
2021-05-07 22:34:54 -0300

    chore: add body-parser to parse requests

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,13 +1,18 @@
 module.exports = () => {
   const express = require('express')
   const consign = require('consign')
+  const bodyParser = require('body-parser')
 
   const app = express()
+
+  // Add bodyParser to receive requests from Json and URL enconded
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
 
   // This metho include all controllers into app
   consign()
     .include('controllers')
     .into(app)
 
-    return app
+  return app
 }
--- controllers/attendance.js
+++ controllers/attendance.js
@@ -1,5 +1,16 @@
 module.exports = app => {
+
+  /**
+   * GET method
+   */
   app.get('/attendances', (req, res) => {
     return res.json({ msg: 'Attendances GET works!' })
+  })
+
+  /**
+   * POST method
+   */
+  app.post('/attendances', (req, res) => {
+    return res.json({ msg: 'Attendances POST works!' })
   })
 }
